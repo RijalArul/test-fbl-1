c2a44036e08defb3da4316d530dcc9b97b58bec5
Bri_Arul <fakhrul.konsultan@work.bri.co.id>
2023-02-15 01:10:51 +0700

    Updated created register Company & Products

--- client/src/features/HomePage.js
+++ client/src/features/HomePage.js
@@ -7,6 +7,8 @@ export default function HomePage () {
   const [transactions, setTransactions] = useState([])
   const [newTransaction, setNewTransaction] = useState({})
   const [companies, setCompanies] = useState([])
+  const [newCompany, setNewCompany] = useState({})
+  const [newProduct, setNewProduct] = useState({})
 
   const accessToken = localStorage.getItem('access_token')
 
@@ -38,13 +40,6 @@ export default function HomePage () {
     getCompanies()
   }, [])
 
-  const handleChange = e => {
-    setNewTransaction({
-      ...newTransaction,
-      [e.target.name]: e.target.value
-    })
-  }
-
   const handleSubmit = async e => {
     e.preventDefault()
     try {
@@ -63,6 +58,75 @@ export default function HomePage () {
       const { data } = resp.data
       setTransactions(...transactions, data)
     } catch (err) {
+      const { error } = err.response.data
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: `${error}`
+      })
+    }
+  }
+
+  const handleChange = e => {
+    setNewTransaction({
+      ...newTransaction,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleChangeNewProduct = e => {
+    setNewProduct({
+      ...newProduct,
+      [e.target.name]: e.target.value
+    })
+  }
+  const handleSubmitProduct = async e => {
+    e.preventDefault()
+    try {
+      newProduct.company_id = parseInt(newProduct.company_id)
+      newProduct.product_price = parseInt(newProduct.product_price)
+      newProduct.product_stock = parseInt(newProduct.product_stock)
+
+      const resp = await axios({
+        method: 'POST',
+        url: 'http://localhost:8080/products',
+        data: newProduct,
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      })
+    } catch (err) {
+      const { error } = err.response.data
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: `${error}`
+      })
+    }
+  }
+
+  const handleChangeNewCompany = e => {
+    setNewCompany({
+      ...newCompany,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmitNewCompany = async e => {
+    e.preventDefault()
+    try {
+      const resp = await axios({
+        method: 'POST',
+        url: 'http://localhost:8080/companies',
+        data: newCompany,
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      })
+
+      const { data } = resp.data
+    } catch (err) {
+      //   console.log(err)
       const { error } = err.response.data
       Swal.fire({
         icon: 'error',
@@ -142,7 +206,7 @@ export default function HomePage () {
             </tr>
           </thead>
           <tbody>
-            {companies.map(company => {
+            {companies?.map(company => {
               return (
                 <tr>
                   <td>{company.id}</td>
@@ -154,7 +218,40 @@ export default function HomePage () {
           </tbody>
         </table>
       </div>
-
+      <div>
+        Add Company
+        <form>
+          <div class='mb-3'>
+            <label for='company_name' class='form-label'>
+              Company Name
+            </label>
+            <input
+              type='text'
+              class='form-control'
+              id='company_name'
+              name='company_name'
+              onChange={e => handleChangeNewCompany(e)}
+            />
+            <label for='company_code' class='form-label'>
+              Company Code
+            </label>
+            <input
+              type='text'
+              class='form-control'
+              id='company_code'
+              name='company_code'
+              onChange={e => handleChangeNewCompany(e)}
+            />
+          </div>
+          <button
+            type='button'
+            class='btn btn-primary'
+            onClick={handleSubmitNewCompany}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
       <div>
         Add Transaction
         <form>
@@ -178,13 +275,16 @@ export default function HomePage () {
                 onChange={e => handleChange(e)}
               >
                 <option selected>-- Please Select Your Company --</option>
-                {companies.map(company => {
-                  return (
-                    <>
-                      <option value={company.id}>{company.company_name}</option>
-                    </>
-                  )
-                })}
+                {companies &&
+                  companies.map(company => {
+                    return (
+                      <>
+                        <option value={company.id}>
+                          {company.company_name}
+                        </option>
+                      </>
+                    )
+                  })}
               </select>
             </div>
 
@@ -199,10 +299,9 @@ export default function HomePage () {
                 <option selected>-- Please Select Your Produc --</option>
                 {newTransaction.company_id === undefined
                   ? null
-                  : companies.map(company => {
+                  : companies?.map(company => {
                       if (parseInt(newTransaction.company_id) == company.id) {
                         return company?.products?.map(product => {
-                          console.log(product)
                           return (
                             <>
                               <option value={product.id}>{product.Name}</option>
@@ -216,6 +315,73 @@ export default function HomePage () {
           </div>
           <div class='mb-3'></div>
           <button type='button' class='btn btn-primary' onClick={handleSubmit}>
+            Submit
+          </button>
+        </form>
+      </div>
+
+      <div>
+        Add Product
+        <form>
+          <div class='mb-3'>
+            <label for='product_name' class='form-label'>
+              Product Name
+            </label>
+            <input
+              type='text'
+              class='form-control'
+              id='product_name'
+              name='product_name'
+              onChange={e => handleChangeNewProduct(e)}
+            />
+            <label for='product_price' class='form-label'>
+              Product Price
+            </label>
+            <input
+              type='number'
+              class='form-control'
+              id='product_price'
+              name='product_price'
+              onChange={e => handleChangeNewProduct(e)}
+            />
+            <label for='product_stock' class='form-label'>
+              Product Stock
+            </label>
+            <input
+              type='number'
+              class='form-control'
+              id='product_stock'
+              name='product_stock'
+              onChange={e => handleChangeNewProduct(e)}
+            />
+            <div className='col-span-6 sm:col-span-3'>
+              <select
+                id='company_id'
+                name='company_id'
+                autoComplete='company_id'
+                className='mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
+                onChange={e => handleChangeNewProduct(e)}
+              >
+                <option selected>-- Please Select Your Company --</option>
+                {companies &&
+                  companies.map(company => {
+                    return (
+                      <>
+                        <option value={company.id}>
+                          {company.company_name}
+                        </option>
+                      </>
+                    )
+                  })}
+              </select>
+            </div>
+          </div>
+          <div class='mb-3'></div>
+          <button
+            type='button'
+            class='btn btn-primary'
+            onClick={handleSubmitProduct}
+          >
             Submit
           </button>
         </form>
